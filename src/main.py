414a116cc0e929d27e87a9d2d6b78c2b88b8787d
fishblish <32463704+fishblish@users.noreply.github.com>
2024-03-03 22:51:47 +0100

    some fixes in filenames

--- src/main.py
+++ src/main.py
@@ -22,8 +22,6 @@ parser.add_argument("--enhancer_regions", type=str, nargs='?', default= data_pat
                         Last column should contain gene names if enhancer is located inside gene, \
                         comma separated, '.' for intergenic enhancers = no gene overlaps.")
 parser.add_argument("--output", type=str, nargs='?', default= "output", help="Name of directory to save outputs in.")
-parser.add_argument("--gtex", type=str, nargs='?', default= data_path + "/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_median_tpm.gct", \
-                    help="Path to directory containing GTEX file (.gct extension).")
 parser.add_argument("--enhancer_activity", type=str, nargs='?', default= data_path + "/h3k27ac_coverage_quantile_normalized.csv", \
                     help="Path to directory containing csv file with enhancer activity.")
 parser.add_argument("--gene_expression", type=str, nargs='?', default= data_path + "/transcrtpts_rnaseq_quantile_normalized.csv", \
@@ -37,12 +35,12 @@ parser.add_argument("--freq_filter_target", type=str, choices=['r', 'c'], defaul
                     help="Choose 'r' (rare) or 'c' (common). It expresses if you want to select rare or common variants considering frequency in population")
 parser.add_argument("--freq_filter_cutoff", type=float, default=0.01, choices=range(0,1), nargs='?', \
                     help="Set cut-off point for frequency filter. It expresses how rare or how common variant you what to select (rare/common depends on freq_filter_target argument)")
-parser.add_argument("--population", type=str, nargs='*', choices=['AFR', 'AMR', 'ASJ', 'EAS', 'FIN', 'NFE', 'OTH'], \
-                    help="Choose population which input data will be copared with. You can pass multiple arguments. If you won't pass any argument, all populations will be used.")
+parser.add_argument("--population", type=str, nargs='*', choices=['AFR', 'AMR', 'ASJ', 'EAS', 'FIN', 'NFE', 'OTH', 'ALL'], default=['AFR', 'AMR', 'ASJ', 'EAS', 'FIN', 'NFE', 'OTH', 'ALL'],\
+                    help="Choose population which input data will be copared with from a list ['AFR', 'AMR', 'ASJ', 'EAS', 'FIN', 'NFE', 'OTH', 'ALL']. You can pass multiple arguments. If you won't pass any argument, whole population frequency will be used.")
 parser.add_argument("--freq_filter_missing", type=str, choices=['r', 'c'], default='c', nargs='?', \
                     help="Choose 'r' (rare) or 'c' (common). It expresses if you want to treat variant with missing frequency data as rare or common variant.")
-parser.add_argument("--reference_population", type=str, choices=['AFR', 'AMR', 'ASJ', 'EAS', 'FIN', 'NFE', 'OTH'], default='ALL', nargs='?', \
-                    help="Choose reference population for binomial test.")
+parser.add_argument("--reference_population", type=str, choices=['AFR', 'AMR', 'ASJ', 'EAS', 'FIN', 'NFE', 'OTH', 'ALL'], default='ALL', nargs='?', \
+                    help="Choose reference population for binomial test. It should be in population list: ['AFR', 'AMR', 'ASJ', 'EAS', 'FIN', 'NFE', 'OTH', 'ALL']. If you won't pass any argument, frequency from whole population will be used.")
 parser.add_argument("--bh_alpha", type=float, default=0.01, choices=range(0,1), nargs='?', \
                     help="Set cut-off point for Benjamini-Hochberg correction.")
 
@@ -57,7 +55,6 @@ ENHANCER_REGIONS = args.enhancer_regions
 OUTPUT = args.output
 ANNOTATED_PROMOTER_SNPs = OUTPUT + "/annotated_promoter_snps.csv"
 ANNOTATED_ENHANCER_SNPs = OUTPUT + "/annotated_enhancer_snps.csv"
-GTEX = args.gtex
 ENHANCER_ACTIVITY = args.enhancer_activity
 GENE_EXPRESSION = args.gene_expression
 CHROMATIN_CONTACTS = args.chromatin_contacts
@@ -81,6 +78,7 @@ except:
 
     print('Directory ', OUTPUT, ' already exist. Output files will be saved in this localization.')
 
+"""  
 #check if ANNOVAR and GATK are installed in provided localization
 flags = fm.check_programs(GATK, ANNOVAR) 
 if flags!= [0,0]:
@@ -88,24 +86,23 @@ if flags!= [0,0]:
     raise TypeError('You should provide correct path to program(s)', list(compress(['GATK', 'ANNOVAR'], flags)))
 
 if fm.prepare_annovar_database(ANNOVAR)==0:
-    raise TypeError(f'ANNOVAR database cound not be installed. You can install it manually from annovar website. It should be placed in {ANNOVAR}/humandb directory.')
+    raise TypeError(f'ANNOVAR database cound not be installed. You can install it manually from annovar website. It should be placed in {ANNOVAR}/humandb directory and unpacked.')
 
 #check if input .vcf file is indexed - it has to be to use some GATK functions
 fm.index_input_vcf(INPUT_VCF, GATK)
 
 # print how many variants are in input file
 fm.count_variants(GATK, INPUT_VCF) 
-
+"""
 #select biallelic variants located inside regulatory regions
 biallelic_variants_in_regulatory_regions_path = fm.select_biallelic_inside_regulatory_regions(GATK, INPUT_VCF, PROMOTER_REGIONS, ENHANCER_REGIONS, OUTPUT)
 
 #found variants that are enriched in analyzed cohort
 annotated_variants_path = fm.annotate_freq(biallelic_variants_in_regulatory_regions_path, ANNOVAR)
-annotated_variants_path = {'promoter': 'output/promoter_SNPs.hg38_multianno.vcf', 'enhancer': 'output/enhancer_SNPs.hg38_multianno.vcf'}
 
 filtered_by_freq_variants_file = fm.select_snps_by_freq(annotated_variants_path, GATK, target=freq_filter_target, cutoff=freq_filter_cutoff, population=population, treating_gaps=freq_filter_missing)
 
-rare_enriched_promoter_snps_df, rare_enriched_enhancer_snps_df = fm.select_enriched_snps(filtered_by_freq_variants_file, GATK, OUTPUT, bh_alpha=bh_alpha)
+rare_enriched_promoter_snps_df, rare_enriched_enhancer_snps_df = fm.select_enriched_snps(filtered_by_freq_variants_file, GATK, bh_alpha=bh_alpha, target=freq_filter_target, reference_population=reference_population)
 
 
 rare_enriched_promoter_snps_gene = fm.assign_genes_to_promoter_snps(rare_enriched_promoter_snps_df, PROMOTER_REGIONS)
@@ -123,11 +120,7 @@ rare_enriched_enhancer_snps_genes_collected = \
 
 rare_enriched_enhancer_snps_genes_collected_corelations = \
     fm.check_signal_gene_expression_correlation_enhancer(rare_enriched_enhancer_snps_genes_collected, ENHANCER_ACTIVITY, GENE_EXPRESSION)
-
-# rare_enriched_promoter_snps_gene, rare_enriched_enhancer_snps_genes_collected_corelations = \
-#     fm.assign_median_tpm_gtex(rare_enriched_promoter_snps_gene, rare_enriched_enhancer_snps_genes_collected_corelations, GTEX)
-
-
+fm.remove_unnecessary_files(OUTPUT)
 promoter_snps_sample_lvl, enhancer_snps_sample_lvl = fm.import_vcf_sample_level(INPUT_VCF, OUTPUT, GATK, rare_enriched_promoter_snps_gene, rare_enriched_enhancer_snps_genes_collected_corelations)
 promoter_snps, enhancer_snps = fm.check_gene_snps_correlation(GENE_EXPRESSION, promoter_snps_sample_lvl, enhancer_snps_sample_lvl)
 fm.visualize_results(promoter_snps, enhancer_snps, GENE_EXPRESSION, OUTPUT)
